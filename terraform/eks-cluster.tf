1490e41f5cfac8b4b08b4758811fc6165765142d
Ben <duychien152003@gmail.com>
2025-07-13 11:51:23 +0700

    update

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.19.1"
+  version = "20.37.1"
 
   cluster_name    = local.cluster_name
   cluster_version = "1.27"
@@ -11,7 +11,6 @@ module "eks" {
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
-
   }
 
   eks_managed_node_groups = {
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,38 +2,35 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.25.0"
+      version = "~> 6.2.0"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.5.1"
+      version = "~> 3.7.2"
     }
 
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 4.0.4"
+      version = "~> 4.1.0"
     }
 
     cloudinit = {
       source  = "hashicorp/cloudinit"
-      version = "~> 2.3.2"
+      version = "~> 2.3.7"
     }
 
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.23.0"
+      version = "~> 2.37.1"
     }
   }
 
   backend "s3" {
     bucket = "gitopsterrastate"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "ap-southeast-1"
   }
 
   required_version = "~> 1.6.3"
 }
-##
-##
-##
